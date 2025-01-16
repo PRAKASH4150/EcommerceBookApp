56b2727559a5cd9ef75ba6be2d6988deeb5d0dd7
PRAKASH4150 <mssprakashnaidu@gmail.com>
2025-01-15 19:53:40 -0500

    Added Edit and Delete page also Server and Client side validations

--- EcommerceBookApp/Controllers/CategoryController.cs
+++ EcommerceBookApp/Controllers/CategoryController.cs
@@ -26,9 +26,75 @@ namespace EcommerceBookApp.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            _applicationDbContext.Categories.Add(category); //Used to persist the Category dataobject in to the table
-            _applicationDbContext.SaveChanges();
-            return RedirectToAction("Index","Category"); // Redirects to the Index in Category
+            if(category.Name !=null && category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order cannot exactly match the name"); //Name is the key
+            }
+           
+			if (ModelState.IsValid)
+            {
+				_applicationDbContext.Categories.Add(category); //Used to persist the Category dataobject in to the table
+				_applicationDbContext.SaveChanges();
+				TempData["success"] = "Category created Sucessfully!!";
+				return RedirectToAction("Index", "Category"); // Redirects to the Index in Category
+			}
+            return View();
         }
-    }
+
+		public IActionResult Edit(int? id)
+		{
+            if(id == null || id==0) {
+                return NotFound();
+            }
+            Category? categoryFromDb = _applicationDbContext.Categories.Find(id); //Find only works on the primary key 
+			//Category? categoryFromDb1 = _applicationDbContext.Categories.FirstOrDefault(u => u.Id == id);//Uses LINQ
+			if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+			return View(categoryFromDb);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_applicationDbContext.Categories.Update(category); //Used to update an existing record
+				_applicationDbContext.SaveChanges();
+				TempData["success"] = "Category updated Sucessfully!!";
+				return RedirectToAction("Index", "Category"); // Redirects to the Index in Category
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _applicationDbContext.Categories.Find(id); //Find only works on the primary key 
+																				  //Category? categoryFromDb1 = _applicationDbContext.Categories.FirstOrDefault(u => u.Id == id);//Uses LINQ
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? categoryObj= _applicationDbContext.Categories.Find(id);
+			if(categoryObj==null)
+			{
+				return NotFound();
+			}
+			_applicationDbContext.Categories.Remove(categoryObj);
+			_applicationDbContext.SaveChanges();
+			TempData["success"] = "Category deleted Sucessfully!!";
+			return RedirectToAction("Index","Category");
+		}
+	}
 }
--- EcommerceBookApp/Models/Category.cs
+++ EcommerceBookApp/Models/Category.cs
@@ -9,9 +9,11 @@ namespace EcommerceBookApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")] //This is the name which will be displayed on the UI.
         public string Name { get; set; }
         [DisplayName("Category Order")]
+        [Range(1,100,ErrorMessage="Display order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
