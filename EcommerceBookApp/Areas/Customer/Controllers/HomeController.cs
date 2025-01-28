7065bc395c43787294bbfb4f013f5292840eb7e1
PRAKASH4150 <mssprakashnaidu@gmail.com>
2025-01-27 21:28:10 -0500

    Added Product Home and Details page

--- EcommerceBookApp/Areas/Customer/Controllers/HomeController.cs
+++ EcommerceBookApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using EcommerceBookApp.DataAccess.Repository.IRepository;
 using EcommerceBookApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,18 +9,29 @@ namespace EcommerceBookApp.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList=_unitOfWork.ProductRepository.GetAll(includeProperties:"Category");
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int productId)
+		{
+			Product product = _unitOfWork.ProductRepository.GetFirstOrDefault(u=> u.Id== productId, includeProperties: "Category");
+			return View(product);
+		}
+
+
+
+		public IActionResult Privacy()
         {
             return View();
         }
